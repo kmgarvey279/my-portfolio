2e5cf9ee6b34ce39313fffd9d3406b4a77420eb5
Kevin Garvey <kmgarvey279@gmail.com>
2020-03-27 15:31:08 -0700

    Updates

---
+++ precache-manifest.f6708d7f20bab3a4da9066c0274807d7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e06c848d2bb201c8e37698b2990e96",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "23f212d1d7f0a4a84d53",
+    "url": "/my-portfolio/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3248bc6ff07664a945da",
+    "url": "/my-portfolio/static/css/main.4a8501d5.chunk.css"
+  },
+  {
+    "revision": "23f212d1d7f0a4a84d53",
+    "url": "/my-portfolio/static/js/2.c6ad99ff.chunk.js"
+  },
+  {
+    "revision": "c48c5930fe1b1b9fb176794f9b6f4eed",
+    "url": "/my-portfolio/static/js/2.c6ad99ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3248bc6ff07664a945da",
+    "url": "/my-portfolio/static/js/main.a620ea10.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  },
+  {
+    "revision": "da33983229bb45d7b2196257bb8754db",
+    "url": "/my-portfolio/static/media/.NET_Core.da339832.png"
+  },
+  {
+    "revision": "1cb0d27ed4379f8fe4c2c33938bfb41e",
+    "url": "/my-portfolio/static/media/Resume.1cb0d27e.pdf"
+  },
+  {
+    "revision": "306327fe7778f5a5fcaad08ba81ddb60",
+    "url": "/my-portfolio/static/media/background (2).306327fe.jpg"
+  },
+  {
+    "revision": "2d7a4bda760feaee457f71ec39ee0a9d",
+    "url": "/my-portfolio/static/media/book1.2d7a4bda.png"
+  },
+  {
+    "revision": "5c0b2c0260c3083e54ac49f65cd4aa6f",
+    "url": "/my-portfolio/static/media/book2.5c0b2c02.png"
+  },
+  {
+    "revision": "32cac56509d4ba446894cef85e91c03c",
+    "url": "/my-portfolio/static/media/book3.32cac565.png"
+  },
+  {
+    "revision": "8744edb71735110e1f566e5f69c09b80",
+    "url": "/my-portfolio/static/media/book4.8744edb7.png"
+  },
+  {
+    "revision": "de83b3acae97cfb5cc771ec5f9d710d5",
+    "url": "/my-portfolio/static/media/c-logo.de83b3ac.png"
+  },
+  {
+    "revision": "a666e207c7f54d4bfd7b5e325ac21283",
+    "url": "/my-portfolio/static/media/cryostat1.a666e207.png"
+  },
+  {
+    "revision": "806ceb985778f9b9e21fd5727b969ccd",
+    "url": "/my-portfolio/static/media/cryostat2.806ceb98.png"
+  },
+  {
+    "revision": "004267e2299ba51d301a9f25ff0ae94f",
+    "url": "/my-portfolio/static/media/cryostat3.004267e2.png"
+  },
+  {
+    "revision": "9472741084ccfd64707700d2e9188801",
+    "url": "/my-portfolio/static/media/cryostat4.94727410.png"
+  },
+  {
+    "revision": "8cc246900eb921368408c505d1b27dd6",
+    "url": "/my-portfolio/static/media/css.8cc24690.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/my-portfolio/static/media/html.2ea01c0e.png"
+  },
+  {
+    "revision": "79046a553ce2e9ea7fc1992ede20e610",
+    "url": "/my-portfolio/static/media/js.79046a55.png"
+  },
+  {
+    "revision": "2fe37d2bdb24d6f9cfdd2258d1f2e6b7",
+    "url": "/my-portfolio/static/media/node.2fe37d2b.png"
+  },
+  {
+    "revision": "35d669a9ea3877bf245e09eb5a650c9e",
+    "url": "/my-portfolio/static/media/photo.35d669a9.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/my-portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/my-portfolio/static/media/redux.c8f44df3.png"
+  },
+  {
+    "revision": "14549ae9ab9deed8de4b309342f6cbea",
+    "url": "/my-portfolio/static/media/robot.14549ae9.gif"
+  },
+  {
+    "revision": "342521a86fd9222f06b714bd070ddb5d",
+    "url": "/my-portfolio/static/media/rpg1.342521a8.png"
+  },
+  {
+    "revision": "b10304989b64f3228f710f77a33c9f5b",
+    "url": "/my-portfolio/static/media/rpg2.b1030498.png"
+  },
+  {
+    "revision": "de85ea733cd33c77214960dcae8a3aab",
+    "url": "/my-portfolio/static/media/rpg3.de85ea73.png"
+  },
+  {
+    "revision": "1a5736ae938fee6f70322bc868c91143",
+    "url": "/my-portfolio/static/media/sql.1a5736ae.png"
+  },
+  {
+    "revision": "94ab6fd533e6bd2a54fe6f4bcf98e266",
+    "url": "/my-portfolio/static/media/ssbu.94ab6fd5.gif"
+  }
+]);
